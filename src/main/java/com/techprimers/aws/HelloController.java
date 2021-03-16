98cf1ff2910518ccc5e4c2d093056fb29d9fc9d6
ShahidSalim-oss <70953311+ShahidSalim-oss@users.noreply.github.com>
2021-03-16 21:08:38 +0500

    Update HelloController.java

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public JSONObject hello() {
-        return new JSONObject("{'name':'helloworld','time':'123123123'}");
+    public String hello() {
+        return "Hello Shahid"+ System.currentTimeMillis();
     }
 
 }
