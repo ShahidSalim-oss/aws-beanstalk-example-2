a157de6ff25cfa1fefa141d19cd2d0ab0a20b23e
ShahidSalim-oss <70953311+ShahidSalim-oss@users.noreply.github.com>
2021-03-18 11:53:31 +0500

    Update HelloController.java

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Hello Shahid"+ System.currentTimeMillis();
+        return "Hello World"+ System.currentTimeMillis();
     }
 }
